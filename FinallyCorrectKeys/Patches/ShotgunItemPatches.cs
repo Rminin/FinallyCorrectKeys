24a7c3de42fa8d991935d781fe87f0576e47113f
Rminin <ijseriuf@gmail.com>
2024-12-17 23:07:41 +0100

    Added patch for the shotgun control tips

---
+++ FinallyCorrectKeys/Patches/ShotgunItemPatches.cs
@@ -0,0 +1,40 @@
+﻿using HarmonyLib;
+using UnityEngine.InputSystem;
+
+namespace FinallyCorrectKeys.Patches;
+
+[HarmonyPatch(typeof(ShotgunItem))]
+public class ShotgunItemPatches
+{
+    private static readonly InputActionAsset actions = IngamePlayerSettings.Instance.playerInput.actions;
+
+    private static readonly string secondaryUseBinding = "ItemSecondaryUse";
+
+    [HarmonyPatch(nameof(ShotgunItem.SetControlTipsForItem))]
+    [HarmonyPostfix]
+    private static void SetControlTipsForItemPatch(ShotgunItem __instance)
+    {
+        __instance.itemProperties.toolTips[2] = __instance.itemProperties.toolTips[2].Replace("[Q]", "[" + GetInputBinding(secondaryUseBinding).ToDisplayString() + "]");
+        FinallyCorrectKeys.Logger.LogDebug("Replaced the " + secondaryUseBinding + " binding inside the ShotgunItem class.");
+        HUDManager.Instance.ChangeControlTipMultiple(__instance.itemProperties.toolTips, holdingItem: true, __instance.itemProperties);
+    }
+
+    [HarmonyPatch(nameof(ShotgunItem.SetSafetyControlTip))]
+    [HarmonyPostfix]
+    private static void SetSafetyControlTipPatch(ShotgunItem __instance)
+    {
+        string changeTo = ((!__instance.safetyOn) ? "Turn safety on: " : "Turn safety off: ");
+        changeTo = changeTo + "[" + GetInputBinding(secondaryUseBinding).ToDisplayString() + "]";
+        if (__instance.IsOwner)
+        {
+            FinallyCorrectKeys.Logger.LogDebug("Inside IsOwner if block.");
+            FinallyCorrectKeys.Logger.LogDebug("Replaced the " + secondaryUseBinding + " binding inside the ShotgunItem class.");
+            HUDManager.Instance.ChangeControlTip(3, changeTo);
+        }
+    }
+
+    private static InputBinding GetInputBinding(string actionName)
+    {
+        return actions.FindAction(actionName).bindings[0];
+    } 
+}
