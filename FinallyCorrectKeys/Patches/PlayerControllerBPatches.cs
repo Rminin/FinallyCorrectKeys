2afd0e5627b4a79077ae2170a0a2748a22d49927
Rminin <ijseriuf@gmail.com>
2024-12-17 03:24:17 +0100

    Added PlayerController patch (interact hover)

---
+++ FinallyCorrectKeys/Patches/PlayerControllerBPatches.cs
@@ -0,0 +1,29 @@
+﻿using GameNetcodeStuff;
+using HarmonyLib;
+using UnityEngine.InputSystem;
+
+namespace FinallyCorrectKeys.Patches;
+
+[HarmonyPatch(typeof(PlayerControllerB))]
+public class PlayerControllerBPatches
+{
+    private static readonly InputActionAsset actions = IngamePlayerSettings.Instance.playerInput.actions;
+
+    private static readonly string interactBinding = "Interact";
+
+    [HarmonyPatch(nameof(PlayerControllerB.SetHoverTipAndCurrentInteractTrigger))]
+    [HarmonyPostfix]
+    private static void SetHoverTipAndCurrentInteractTriggerPatch(PlayerControllerB __instance)
+    {
+        var cursorTip = __instance.cursorTip;
+        if (cursorTip.text.Contains("[E]"))
+        {
+            cursorTip.text = cursorTip.text.Replace("[E]", "[" + GetInputBinding(interactBinding).ToDisplayString() + "]");
+            FinallyCorrectKeys.Logger.LogDebug("Replaced the " + interactBinding + " binding.");
+        }
+    }
+    private static InputBinding GetInputBinding(string actionName)
+    {
+        return actions.FindAction(actionName).bindings[0];
+    }
+}
