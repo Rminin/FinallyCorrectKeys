a573c552ec0c7bf70c828b853fd7feeab4c3661d
Rminin <ijseriuf@gmail.com>
2025-02-08 11:57:10 +0100

    Add standard key property to BindingName

--- FinallyCorrectKeys/Util/BindingName.cs
+++ FinallyCorrectKeys/Util/BindingName.cs
@@ -2,28 +2,35 @@
 
 public class BindingName
 {
-    private readonly string _typeKeyWord;
+    public readonly string Name;
+    public readonly string StandardKey;
 
-    private BindingName(string typeKeyWord)
+    private BindingName(string name, string standardKey)
     {
-        _typeKeyWord = typeKeyWord;
-    } 
+        Name = name;
+        StandardKey = standardKey;
+    }
+
+    public string ToHUDFormat()
+    {
+        return "[" + StandardKey + "]";
+    }
 
     public override string ToString()
     {
-        return _typeKeyWord;
+        return Name;
     }
 
-    public static readonly BindingName DISCARD = new("Discard");
-    public static readonly BindingName USE = new("Use"); // Don't know where it gets used
-    public static readonly BindingName ACTIVATE_ITEM = new("ActivateItem");
-    public static readonly BindingName SECONDARY_USE = new("ItemSecondaryUse");
-    public static readonly BindingName TERTIARY_USE = new("ItemTertiaryUse");
-    public static readonly BindingName INSPECT_ITEM = new("InspectItem");
-    public static readonly BindingName SPRINT = new("Sprint");
-    public static readonly BindingName SCAN = new("PingScan");
-    public static readonly BindingName INTERACT = new("Interact");
-    public static readonly BindingName BUILD = new("BuildMode");
-    public static readonly BindingName ROTATE = new("ReloadBatteries");
-    public static readonly BindingName STORE = new("Delete");
+    public static readonly BindingName DISCARD = new("Discard", "G");
+    public static readonly BindingName USE = new("Use", "LMB"); // Don't know where it gets used
+    public static readonly BindingName ACTIVATE_ITEM = new("ActivateItem", "LMB");
+    public static readonly BindingName SECONDARY_USE = new("ItemSecondaryUse", "Q");
+    public static readonly BindingName TERTIARY_USE = new("ItemTertiaryUse", "E");
+    public static readonly BindingName INSPECT_ITEM = new("InspectItem", "Z");
+    public static readonly BindingName SPRINT = new("Sprint", "Shift");
+    public static readonly BindingName SCAN = new("PingScan", "RMB");
+    public static readonly BindingName INTERACT = new("Interact", "E");
+    public static readonly BindingName BUILD = new("BuildMode", "B");
+    public static readonly BindingName ROTATE = new("ReloadBatteries", "R");
+    public static readonly BindingName STORE = new("Delete", "X");
 }
